d858fe157edd1dd849bfdee609e627bcde12ac59
rohnshrma <rohn11shrma@gmail.com>
2025-11-10 17:58:45 +0530

    js session 1

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// output
+
+// 1. console
+// console.log("hello world");
+// console.log(34 + 34);
+
+// 2. alert
+// window.alert("hello world");
+// alert("hello world");
+
+// 3. document.write()
+
+// document.write("hello world");
+
+// input
+
+// prompt("Enter your name : ", "john doe");
+
+// variables : empty containers used to store data, having a label on them
+// known as variable name | identifier
+
+// how to create a variable
+
+// declaration
+
+// "var" keyword is used to declare a variable followed by variable name
+
+var age; // declaration
+age = 22; // initialization
+
+var yourage = 34; // declaration and  initialization
+
+console.log(yourage);
+
+yourage = 100;
+
+console.log(yourage);
+
+// var myage = prompt("enter your age :");
+
+// rules
+
+// 1. no space
+// 2. a variable name cannot start with a number, but can include or end with a number
+// 3. a variable name cannot start, include or end with a special symbol , except "_" and "$"
+// 4. do not used reserved keyword as variable name
+
+// suggestion
+// 1. start variable name with lowercase letter
+// 2. in case of multiword variable name, use
+// --- camelCasing
+// --- snake_casing
+
+var helloworldmynameis;
+var helloWorldMyNameIs; // camelCasing
+var hello_world_my_name_is; // snake_casing
